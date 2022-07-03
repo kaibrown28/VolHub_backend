61c4f3fb17821bb437ee72b308f655a0b729d767
kbrpronet <kaibrown2889@gmail.com>
2022-07-03 11:55:26 -0400

    adjusted db file

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const Pool = require("pg").Pool
+require("dotenv").config
 
-const pool = new Pool ({
-    user: process.env.USER_NAME,
-    password: process.env.PASSWORD ,
-    host: process.env.DATABASE_URL,
-    port: 5432,
-    database: process.env.DATABASE_NAME
-})
 
-module.exports = pool
+const devConfig={
+    user: process.env.PG_NAME,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE
+};
+
+const proConfig ={
+    connectionString: process.env.DATABASE_URL
+};
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig)
+
+module.exports = pool(devConfig)
