8e3736a045e24694c908051fd49445f053fc3b02
kbrpronet <kaibrown2889@gmail.com>
2022-07-05 12:49:13 -0400

    merging changes to projects and volunteer.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 // const volunteer =  require("./controllers/volunteer")
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 //Middleware
 app.use(bodyParser.json())
